08a675047aafcd15236f8da7b8cb25e8ac9dafc0
Christian Ment <christian.paul.ment@gmail.com>
2023-01-31 12:42:49 +0100

    language support

---
+++ script.js
@@ -0,0 +1,21 @@
+function langDE() {
+  document.getElementById("motto").innerHTML = "Der beste Ramen in der Stadt";
+  document.getElementById("howto").innerHTML = "So bestellst Du";
+  document.getElementById("cta1").innerHTML = "Wähle deine Nudeln aus";
+  document.getElementById("cta2").innerHTML = "Wähle deine Geschmacksrichtung";
+  document.getElementById("cta3").innerHTML = "Füge Beilagen hinzu";
+  document.getElementById("menumain").innerHTML = "Unser MenÜ";
+  document.getElementById("menuvariation1").innerHTML = "Hühnchen";
+  document.getElementById("menuvariation2").innerHTML = "Rind";
+  document.getElementById("menuvariation3").innerHTML = "Schrimps";
+  document.getElementById("menuvariation4").innerHTML = "Hühnchen";
+  document.getElementById("menuvariation5").innerHTML = "Rind";
+  document.getElementById("menuvariation6").innerHTML = "Schrimps";
+  document.getElementById("menuvariation7").innerHTML = "Hühnchen";
+  document.getElementById("menuvariation8").innerHTML = "Rind";
+  document.getElementById("menuvariation8").innerHTML = "Schrimps";
+  document.getElementById("find").innerHTML = "So findest Du zu uns";
+  document.getElementById("nav1").innerHTML = "Menü";
+  document.getElementById("nav2").innerHTML = "Standort";
+  document.getElementById("nav3").innerHTML = "Über uns";
+}
